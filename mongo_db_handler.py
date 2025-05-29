7cdf465a4451d3a281859b8b3b2fe0679c9ad62d
mohamadaboria <mohamadaboria116@gmail.com>
2025-05-30 02:51:55 +0300

    updated mongo handler

--- mongo_db_handler.py
+++ mongo_db_handler.py
@@ -1,4 +1,7 @@
 from pymongo.mongo_client import MongoClient
+import pymongo
+import time
+import datetime
 # from pymongo.server_api import ServerApi
 
 
@@ -17,17 +20,21 @@ class MongoDBHandler:
 
     def sensor_field_doc_temp(self, sensor_id, sensor_type, sensor_value, sensor_unit):
         return {
+            '_id': '',
             'sensor_id': sensor_id,
             'sensor_type': sensor_type,
             'sensor_value': sensor_value,
-            'sensor_unit': sensor_unit
+            'sensor_unit': sensor_unit,
+            'timestamp': ''
         }
     
     def actuator_field_doc_temp(self, actuator_id, actuator_type, actuator_value):
         return {
+            '_id': '',
             'actuator_id': actuator_id,
             'actuator_type': actuator_type,
-            'actuator_value': actuator_value
+            'actuator_value': actuator_value,
+            'timestamp': ''
         }
 
     def create_collection(self, collection_name, key, fields):
@@ -37,31 +44,37 @@ class MongoDBHandler:
         }
 
     def insert_sensor_data(self, key, sensor_value):
-        self.__pi_data_map[key]['fields']['sensor_value'] = sensor_value
-        self.__pi_data_map[key]['collection'].insert_one(self.__pi_data_map[key]['fields'])
+        try:
+            timestamp_val = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.__pi_data_map[key]['fields']['_id'] = self.__pi_data_map[key]['fields']['sensor_id'] + timestamp_val.replace(" ", "").replace(":", "").replace("-", "") + str(int(time.time())) 
+            self.__pi_data_map[key]['fields']['sensor_value'] = sensor_value
+            self.__pi_data_map[key]['fields']['timestamp'] = timestamp_val
+            self.__pi_data_map[key]['collection'].insert_one(self.__pi_data_map[key]['fields'])
+            return True
+        except Exception as e:
+            print(f"Error inserting sensor data: {e}")
+            return False
 
     def insert_actuator_data(self, key, actuator_value):
-        self.__pi_data_map[key]['fields']['actuator_value'] = actuator_value
-        self.__pi_data_map[key]['collection'].insert_one(self.__pi_data_map[key]['fields'])
+        try:
+            timestamp_val = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.__pi_data_map[key]['fields']['_id'] = self.__pi_data_map[key]['fields']['actuator_id'] + timestamp_val.replace(" ", "").replace(":", "").replace("-", "") + str(int(time.time()))
+            self.__pi_data_map[key]['fields']['actuator_value'] = actuator_value
+            self.__pi_data_map[key]['fields']['timestamp'] = timestamp_val
+            self.__pi_data_map[key]['collection'].insert_one(self.__pi_data_map[key]['fields'])
+            return True
+        except Exception as e:
+            print(f"Error inserting actuator data: {e}")
+            return False
 
-    def insert_image_data(self, key, image_path, timestamp):
-        self.__pi_data_map[key]['fields']['image'] = image_path
-        self.__pi_data_map[key]['fields']['timestamp'] = timestamp
-        self.__pi_data_map[key]['collection'].insert_one(self.__pi_data_map[key]['fields'])
-
-    def get_data(self, key):
-        return self.__pi_data_map[key]['collection'].find_one()
-    
-    def get_all_data(self, key):
-        return self.__pi_data_map[key]['collection'].find()
-    
-    def delete_data(self, key):
-        self.__pi_data_map[key]['collection'].delete_many({})
-
-    def delete_all_data(self):
-        self.__db.drop()
-
-    def close_connection(self):
-        self.__client.close()
-
-    
+    def insert_image_data(self, key, image_path, cam_id = 0):
+        try:
+            timestamp_val = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.__pi_data_map[key]['fields']['_id'] = f'image_c{cam_id}' + timestamp_val.replace(" ", "").replace(":", "").replace("-", "") + str(int(time.time()))
+            self.__pi_data_map[key]['fields']['image'] = image_path
+            self.__pi_data_map[key]['fields']['timestamp'] = timestamp_val
+            self.__pi_data_map[key]['collection'].insert_one(self.__pi_data_map[key]['fields'])
+            return True
+        except Exception as e:
+            print(f"Error inserting image data: {e}")
+            return False
